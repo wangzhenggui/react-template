eb2280d660d8e64f086d158123600c5df33f9d93
wangzhenggui <13588067554@163.com>
2021-02-28 22:12:01 +0800

    fix: husky测试

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const arr1 = [
-  [0, 1],
-  [2, 3],
-  [4, 5],
-];
 ReactDOM.render(
   <React.StrictMode>
     <App />
